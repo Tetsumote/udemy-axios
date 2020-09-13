1a1727c7d925e8f6044fa516c8e7077dc5de0d59
Paweł Grajewski <unionproject88@gmail.com>
2020-09-13 21:11:53 +0200

    add axios configuration

--- src/index.js
+++ src/index.js
@@ -2,6 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
+axios.interceptors.request.use(
+  (request) => {
+    console.log(request);
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    console.log(response);
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
